3fe6ffca18db0940964da1ed9633ca11cc25f46a
roypromax <roy19rishabh@gmail.com>
2023-07-14 05:18:12 +0530

    hello world with vue

---
+++ hello-world/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>Hello World!</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+};
+</script>
+
+<style scoped>
+h2 {
+  color: green;
+}
+</style>
